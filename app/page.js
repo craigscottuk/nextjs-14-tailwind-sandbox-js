3dd94f0592cb7d08060c6cc38bb34a08d623a9b3
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2024-02-03 14:21:08 +0100

    Adding Prettier plugin for Tailwind CSS.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-center h-screen bg-slate-50'>
+    <main className='flex h-screen flex-col items-center justify-center bg-slate-50'>
       <h1 className='text-3xl font-semibold text-slate-900'>
         Next.js 14 and Tailwind CSS in a JavaScript Sandbox
       </h1>
     </main>
-  );
+  )
 }
